fd21f3fe08687c975c010d9f7e263bf2f28bf0f9
wjforeroo <wjforeroph@gmail.com>
2024-08-15 11:31:51 -0500

    Se agregan los test 1 y 2

--- tests/test_3.spec.ts
+++ tests/test_3.spec.ts
@@ -21,3 +21,32 @@ test('Realizar una busqueda que genere al menos tenga un resultado', async ({ pa
   await expect(numberOfResults).toBeGreaterThan(0);
 
 });
+
+test('Realizar una busqueda que no tenga resultados', async ({ page }) => {
+  
+  await page.getByLabel('Search').click();
+
+  await page.getByPlaceholder('Search docs').click();
+
+  await page.getByPlaceholder('Search docs').fill('hascontent');
+
+  await expect(page.locator('.DocSearch-NoResults p')).toBeVisible();
+
+  await expect(page.locator('.DocSearch-NoResults p')).toHaveText('No results for "hascontent"');
+});
+
+test('Limpiar el input de busqueda', async ({ page }) => {
+  await page.getByRole('button', { name: 'Search' }).click();
+
+  const searchBox = page.getByPlaceholder('Search docs');
+
+  await searchBox.click();
+
+  await searchBox.fill('somerandomtext');
+
+  await expect(searchBox).toHaveValue('somerandomtext');
+
+  await page.getByRole('button', { name: 'Clear the query' }).click();
+
+  await expect(searchBox).toHaveAttribute('value', '');
+});
